19ad517758f11c587349615a1e0f323792e5bb64
Pradeep Kushwaha <prk.shine@gmail.com>
2020-05-16 22:50:40 +0530

    added shop page and related components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 
 
@@ -10,7 +11,7 @@ function App() {
     <div>
       <Switch>
      <Route exact path='/' component={HomePage} /> 
-     
+    <Route path="/shop" component={ShopPage} />
      </Switch>
     </div>
   );
